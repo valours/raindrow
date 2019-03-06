87407ee2c37180d9ccc5ed5d5718572a64eeaabc
Valentin Le FLem <leflem.valentin@gmail.com>
2019-03-06 17:44:43 +0100

    extract styles counteralpha

--- src/containers/CounterAlpha/index.js
+++ src/containers/CounterAlpha/index.js
@@ -1,40 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import Square from '../../components/Square';
 import counterHook from '../../hooks/counter';
+import styles from './styles';
 
-const Card = styled(Square)`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  color: white;
-  background: #2193b0;  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #6dd5ed, #2193b0);  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #6dd5ed, #2193b0); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-`
-
-const Count = styled.div`
-  font-size: 35px;
-  font-weight: bold;
-  padding: 8px;
-`
-
-const Buttons = styled.div`
-  display: flex;
-  justify-content: center;
-  color: #2193b0;
-  font-size: 22px;
-  background: white;
-  max-width: 100px;
-  padding: 8px;
-  border-radius: 10px;
-`
-
-const Button = styled.div`
-  border; 1px solid #2193b0;
-`
+const { Card, Count, Buttons, Button } = styles;
 
 export default () => {
   const { add, take, count } = counterHook();
