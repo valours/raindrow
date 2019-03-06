b1e6d1674da91a2113d3d99ab5aa2c69a56ed3a9
Valentin Le FLem <leflem.valentin@gmail.com>
2019-03-06 17:55:50 +0100

    template - fix with new components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+
 import Header from './components/Header';
+import Section from './components/Section';
+
 import CounterAlpha from './containers/CounterAlpha';
+import CounterBeta from './containers/CounterBeta';
 import TodoAlpha from './containers/TodoAlpha';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <CounterAlpha />
+        <Section title='Section'>
+          <CounterAlpha />
+          <CounterBeta />
+        </Section>
         <TodoAlpha />
       </div>
     );
