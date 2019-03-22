eb2a8043d5e7435f018f92f8b6eeb32a7a3d065b
Valentin Le FLem <leflem.valentin@gmail.com>
2019-03-22 18:21:12 +0100

    motion upgrade

--- src/containers/Muddle/FAQAlpha/index.js
+++ src/containers/Muddle/FAQAlpha/index.js
@@ -5,20 +5,24 @@ const Question = styled.div`
   margin: 10px;
   border-radius: 4px;
   transition: background 1.2s;
-  background: ${props => props.focused ? 'white' : '#3855df'}
+  max-width: 260px;
+  background: ${props => props.focused ? '#eee' : '#3855df'}
 
   & > .title {
+    font-size: 1.1em;
     padding: 10px;
     transition: color 1s;
     color: ${props => props.focused ? '#3855df' : 'white'}
   }
 
   & > .answer {
-    color: red;
-    max-width: 50px;
+    padding: 20px;
     transition: height 1s;
+    transition: color 1s;
     overflow: hidden;
-    height: ${props => !props.focused ? '0px' : '100px'};
+    padding: ${props => props.focused ? '0 10px 10px 10px' : '0'};
+    color: ${props => props.focused ? '#3855df' : 'white'};
+    height: ${props => !props.focused ? '0px' : '75px'};
   }
 `
 
@@ -29,21 +33,24 @@ const FAQAlpha = () => {
       {questionFocused}
       <Question
         focused={questionFocused === 1}
-        onClick={() => setQuestionFocused(1)}
+        onClick={() => setQuestionFocused(questionFocused === 1 ? 0 : 1)}
       >
         <div className='title'>
           How do I contact support?
         </div>
+        <div className='answer'>
+        To unzip a single file or folder, open the zipped folder, then drag the file or folder from the zipped folder to a new location.
+        </div>
       </Question>
       <Question
         focused={questionFocused === 2}
-        onClick={() => setQuestionFocused(2)}
+        onClick={() => setQuestionFocused(questionFocused === 2 ? 0 : 2)}
       >
         <div className='title'>
           How I can unzip product files?
         </div>
         <div className='answer'>
-          truc truc truc truc truc truc truc truc truc;
+        To unzip a single file or folder, open the zipped folder, then drag the file or folder from the zipped folder to a new location.
         </div>
       </Question>
     </div>
